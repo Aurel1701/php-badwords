fee95afad2a171ce2b9927f0ce1a7d6c657aa7d0
Aurel1701 <gantzfan96@gmail.com>
2022-05-19 14:04:39 +0200

    edit new-text

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
 <?php
 // creo una variabile con lorem 
 $text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, aliquid?';
 // tramite sterl mi darà la lunghezza dei caratteri che prendono gli spazi chiusi 
 echo strlen($text);
+// gli passo la parola che decido di censurare con $_GET
+$censured = $_GET['Lorem'];
+
+$new_text = (str_replace($censured, '***', $text));
+
 ?>
 
 <!DOCTYPE html>
@@ -16,7 +26,15 @@ echo strlen($text);
 <body>
     <p>
         <!-- stampo a schermo tramite l'echo -->
-         <?php echo $text ?>
+         <?php echo $text; ?>
+    </p>
+
+    <p>
+
+    <?php echo $new_text;
+    echo strlen($new_text);  ?>
+    <!-- nuova lunghezza del paragrafo -->
+    
     </p>
 </body>
 </html>
